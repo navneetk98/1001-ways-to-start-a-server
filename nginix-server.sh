f3e0da6168d9bd1023fbce55b1971c72b970b3ae
navneetkhandelwal0 <85572093+navneetkhandelwal0@users.noreply.github.com>
2021-10-28 13:44:09 +0530

    added nginix conf files

---
+++ nginix-server.sh
@@ -0,0 +1,15 @@
+cd /etc/nginx/sites-enabled
+sudo "${EDITOR:-vi}" tutorial
+server {
+       listen 81;
+       listen [::]:81;
+
+       server_name example.ubuntu.com;
+
+       root /var/www/tutorial;
+       index index.html;
+
+       location / {
+               try_files $uri $uri/ =404;
+       }
+}
